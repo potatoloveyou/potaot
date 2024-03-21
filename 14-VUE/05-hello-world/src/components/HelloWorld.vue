efeac6ffeeb512ea7030f7f75240eadd54f3006d
potato <2444265157@qq.com>
2024-03-21 19:45:38 +0800

    第一次尝试VUE脚手架

---
+++ 14-VUE/05-hello-world/src/components/HelloWorld.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <ul>
+      <li v-for="(num, index) in nums" :key="index">{{ num }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  // props: {
+  //   msg: String
+  // }
+  props: ['msg', 'nums'],
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+a {
+  color: #42b983;
+}
+</style>
---
+++ 14-VUE/05-hello-world/src/components/TestModule.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="test">
+    <h1>123</h1>
+    <p>msg:{{ msg }}</p>
+  </div>
+</template>
+<script>
+export default {
+  props: ['msg'],
+};
+</script>
+<style lang=""></style>

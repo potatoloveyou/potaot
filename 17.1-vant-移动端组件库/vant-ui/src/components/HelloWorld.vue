cd0cc0542bf57e3320d98cad1ccb80bddbe4440d
potato <2444265157@qq.com>
2024-06-14 11:16:05 +0800

    使用vant自动按需引入插件

--- 17.1-vant-移动端组件库/vant-ui/src/components/HelloWorld.vue
+++ 17.1-vant-移动端组件库/vant-ui/src/components/HelloWorld.vue
@@ -1,11 +1,11 @@
 <template>
 	<div class="hello">
 		<h1>{{ msg }}</h1>
-		123
-		<van-rate v-model="value" />
 		<van-button type="primary">主要按钮</van-button>
 		<van-button type="info">信息按钮</van-button>
 		<van-button type="default">默认按钮</van-button>
+		<van-button type="warning">警告按钮</van-button>
+		<van-button type="danger">危险按钮</van-button>
 	</div>
 </template>
 
@@ -15,13 +15,7 @@ export default {
 	props: {
 		msg: String,
 	},
-	data() {
-		return {
-			value: 3,
-		};
-	},
 };
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss"></style>
--- 17.1-vant-移动端组件库/vant-ui/src/main.js
+++ 17.1-vant-移动端组件库/vant-ui/src/main.js
@@ -3,25 +3,12 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 
-// 全部引入
-// import Vant from 'vant';
-// import 'vant/lib/index.css';
-// Vue.use(Vant);
-
-// // 按需引入
-// import { Rate } from 'vant';
-// import 'vant/lib/rate/style';
-// Vue.use(Rate);
-
-// import { Button } from 'vant';
-// import 'vant/lib/button/style';
-// Vue.use(Button);
-
-import { Rate, Button } from 'vant';
-import 'vant/lib/index.css';
-Vue.use(Rate);
+// 方式一：自动按需引入vant组件
+// （1）下载 babel-plugin-import 插件：npm install babel-plugin-import --save-dev。
+// （2）在 .babelrc 或 babel.config.js 中配置插件：{ "plugins": [["import", { "libraryName": "vant", "style": true }]] }。
+// （3）在main.js中自动引入所需组件并注册(需要重启)
+import { Button } from 'vant';
 Vue.use(Button);
-
 
 Vue.config.productionTip = false;
 

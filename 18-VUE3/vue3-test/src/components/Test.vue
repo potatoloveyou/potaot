ca26b13228138b0457fcae61f190ab3ece3e2f02
potato <2444265157@qq.com>
2024-06-15 20:51:59 +0800

    Vue3初尝试

---
+++ 18-VUE3/vue3-test/src/components/Test.vue
@@ -0,0 +1,21 @@
+<template>
+	<div>
+		<h1>我是test</h1>
+	</div>
+</template>
+
+<script>
+export default {
+	// 组合式写法写在setup里
+	setup() {
+		return {};
+	},
+
+	// 若是还是想用选项式写法就写在setup外
+	data() {
+		return {};
+	},
+};
+</script>
+
+<style lang="scss" scoped></style>
---
+++ 18-VUE3/vue3-test/src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<template>
+	<main>
+		<HelloWorld msg="test"></HelloWorld>
+		<Test />
+	</main>
+</template>
+
+<script setup>
+import HelloWorld from '@/components/HelloWorld.vue';
+import Test from '@/components/Test.vue';
+</script>

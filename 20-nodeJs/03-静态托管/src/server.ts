b8aa8dbaaf68c1c40ecd0cd5d7d4a614e635575b
potato <2444265157@qq.com>
2024-10-09 17:48:32 +0800

    koa静态资源托管

---
+++ 20-nodeJs/03-静态托管/src/server.ts
@@ -0,0 +1,49 @@
+const Koa = require('koa');
+const app = new Koa();
+
+// 处理静态资源
+const KoaStatic = require('koa-static');
+// https://github.com/koajs/static
+app.use(KoaStatic('public'));
+
+// 处理路由
+const Router = require('@koa/router');
+// https://github.com/koajs/router
+const router = new Router();
+
+// node原生fs/promises模块
+// const fs = require('fs/promises');
+// app.use(async (ctx, next) => {
+// 	try {
+// 		let res = await fs.readFile('public/' + ctx.url);
+// 		ctx.body = res;
+// 	} catch (error) {
+// 		await next();
+// 	}
+// });
+
+// app.use(async (ctx, next) => {
+// 	ctx.body = { name: 'nick', age: 18 };
+// });
+
+// router.get("/", func)
+// GET方式  /路径  响应函数
+router
+	.get('/', (ctx, next ) => {
+		ctx.body = '首页';
+	})
+	.get('/about', (ctx, next) => {
+		ctx.body = 'about 页面 GET';
+	})
+	.post('/about', (ctx, next) => {
+		ctx.body = 'about 页面 POST';
+	})
+	.get('/user/:id', (ctx, next) => {
+		ctx.body = 'user 页面 GET';
+		console.log(ctx.params.id);
+	});
+// GET params传参 /user/123
+
+
+app.use(router.routes());
+app.listen(8080);

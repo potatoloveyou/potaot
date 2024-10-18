2d657cd1ba5357a4182997cb21a3e06b22ff5854
potato <2444265157@qq.com>
2024-10-18 22:23:38 +0800

    解决nuxt安装@element-plus/nuxt浏览器报错问题(需单独安装一个dayjs依赖)

---
+++ 22.nuxt/01-nuxt初尝试/nuxt3-app/app.vue
@@ -0,0 +1,33 @@
+<template>
+	<div>{{ name }}</div>
+	<button @click="test">测试1</button>
+	<h1>你好</h1>
+	<p>222</p>
+
+	<el-button type="primary">测试</el-button>
+</template>
+
+<script setup>
+	const name = ref('aaa');
+
+	const config = useRuntimeConfig();
+	// console.log(config.public.baseUrl);
+	// console.log(config.count);
+
+	const test = () => {
+		console.log('test');
+	};
+
+	if(import.meta.server){
+		console.log('服务端');
+	}else{
+		console.log('客户端');
+	}
+	
+</script>
+
+<style lang="scss" scoped>
+	p {
+		color: $myP;
+	}
+</style>

60d979b2f9fae064cda4fcf93387cd60ce026303
potato <2444265157@qq.com>
2025-10-20 22:43:59 +0800

    删除无用项目与添加react厕所项目

---
+++ 23-React/react_test/src/App.tsx
@@ -0,0 +1,20 @@
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+	const [count, setCount] = useState(0);
+
+	return (
+		<>
+			<div className='card'>
+				<button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
+				<p>
+					Edit <code>src/App.tsx</code> and save to test HMR
+				</p>
+			</div>
+			<div className='text-red-500'>测试tailwindcss</div>
+		</>
+	);
+}
+
+export default App;

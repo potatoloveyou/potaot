002bb1380faec6dba00801c60eb05c946b04cc91
potato <2444265157@qq.com>
2025-12-12 00:35:12 +0800

    🐞 fix(私信弹出层): 修复私信弹出层层级问题

--- 18-VUE3/potato-blog-manage/src/layout/DefaultLayout.vue
+++ 18-VUE3/potato-blog-manage/src/layout/DefaultLayout.vue
@@ -1,7 +1,7 @@
 <template>
 	<div class="default-layout h-dvh">
 		<el-container class="h-full">
-			<el-header height="4rem" class="w-full px-4 fixed top-0 left-0 bg-white grid items-center default-layout-header-theme">
+			<el-header height="4rem" class="w-full px-4 fixed top-0 left-0 z-10 bg-white grid items-center default-layout-header-theme">
 				<AppHeader />
 			</el-header>
 

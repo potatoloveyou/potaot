b1aaa3b6e024207a7ff1a921ea8108d010a99dac
potato <2444265157@qq.com>
2026-01-07 17:31:40 +0800

    ✨ feat(NoteList): 添加图片预览和删除功能
    - 为图片添加点击预览功能，限制显示前3张图片
    - 新增删除按钮，悬停显示并支持确认删除操作
    - 引入el-image-viewer组件实现图片预览

--- 18-VUE3/potato-blog-manage/src/components/HandwrittenNotesView/NoteList.vue
+++ 18-VUE3/potato-blog-manage/src/components/HandwrittenNotesView/NoteList.vue
@@ -9,26 +9,40 @@
 		<template #default="{ item }">
 			<DynamicScrollerItem :item active>
 				<div class="pb-4">
-					<WhiteContainer>
+					<WhiteContainer class="group">
 						<div class="w-full flex flex-col justify-center [&>*]:w-full">
 							<el-text class="text-xl font-semibold text-[#1E2025] mb-2">{{ item.title }}</el-text>
 							<el-text size="small" type="info" class="mx-4 mb-4">{{ item.createTime }}</el-text>
 							<el-text class="text-base">{{ item.introduce }}</el-text>
 						</div>
 						<div class="mt-4">
-							<el-image v-for="value in item.picture" :src="value" lazy fit="scale-down" class="w-22 h-22 mr-4" />
+							<el-image
+								v-for="value in item.picture.slice(0, 3)"
+								:src="value"
+								lazy
+								fit="scale-down"
+								@click="handlePreviewClick(item.picture)"
+								class="w-22 h-22 mr-4" />
 						</div>
+						<el-icon
+							class="absolute top-6 right-6 cursor-pointer opacity-0 group-hover:opacity-100"
+							@click="changeDel(item.id)"
+							><Delete
+						/></el-icon>
 					</WhiteContainer>
 				</div>
 			</DynamicScrollerItem>
 		</template>
 	</DynamicScroller>
+	<el-image-viewer v-if="showPreview" :url-list="previewList" @close="showPreview = false" show-progress />
 </template>
 
 <script setup lang="ts">
 import { ref } from 'vue';
 
 import type { DiaryType, DiaryItemType } from '@/type/diary.type';
+
+import { Delete } from '@element-plus/icons-vue';
 
 import { DynamicScroller, DynamicScrollerItem } from 'vue-virtual-scroller';
 import 'vue-virtual-scroller/dist/vue-virtual-scroller.css';
@@ -44,8 +58,29 @@ interface NoteListProps {
 }
 const { data } = defineProps<NoteListProps>();
 
-const limit = defineModel<number>('limit', { default: 5 });
-const page = defineModel<number>('page', { default: 1 });
+const changeDel = (id: number | string) => {
+	ElMessageBox.confirm('确定删除该评论吗？', '提示', {
+		confirmButtonText: '确定',
+		cancelButtonText: '取消',
+		type: 'error',
+		icon: markRaw(Delete),
+	})
+		.then(() => {
+			// ElMessage.success('删除成功');
+			console.log('删除成功', id);
+		})
+		.catch(() => {
+			console.log('取消删除');
+		});
+};
+
+const showPreview = ref(false);
+const previewList = ref<string[]>([]);
+// 点击预览
+const handlePreviewClick = (urlList: string[]) => {
+	showPreview.value = true;
+	previewList.value = urlList;
+};
 </script>
 
 <style lang="scss" scoped>

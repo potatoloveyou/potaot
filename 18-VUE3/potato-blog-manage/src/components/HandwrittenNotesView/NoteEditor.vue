23869d49bfcb9bdfb7cf4738dbda3a80a9f18dab
potato <2444265157@qq.com>
2026-01-13 01:02:00 +0800

    ✨ feat(NoteEditor): 添加图片上传和预览功能
    - 在笔记编辑器中增加图片上传组件
    - 实现图片预览、下载和删除功能
    - 优化标题输入框的清除按钮交互
    - 调整编辑器高度和图片卡片样式

--- 18-VUE3/potato-blog-manage/src/components/HandwrittenNotesView/NoteEditor.vue
+++ 18-VUE3/potato-blog-manage/src/components/HandwrittenNotesView/NoteEditor.vue
@@ -1,8 +1,34 @@
 <template>
-	<WhiteContainer class="pr-0">
-		<el-input v-model.trim="rubric" clearable placeholder="请输入标题" class="text-2xl mb-2" />
+	<WhiteContainer class="px-3 py-3">
+		<el-input v-model.trim="rubric" placeholder="请输入标题" class="text-2xl h-8 group mb-2">
+			<template #suffix>
+				<el-icon
+					@click="resetRubric"
+					class="text-xl cursor-pointer opacity-0"
+					:class="{ 'group-hover:opacity-100': rubric.length > 0 }"
+					><CircleClose
+				/></el-icon>
+			</template>
+		</el-input>
 		<MdEditor v-model="content" :preview="false" :toolbars="[]" placeholder="请输入内容" />
-		<div class="flex justify-end mt-4">
+
+		<el-upload action="#" multiple list-type="picture-card" :auto-upload="false" class="my-2">
+			<el-icon><Plus /></el-icon>
+			<template #file="{ file }">
+				<div class="w-24 h-24">
+					<el-image fit="scale-down" :src="file.url" />
+					<span class="el-upload-list__item-actions justify-evenly [&>i]:cursor-pointer">
+						<el-icon @click="handlePreviewClick(file)"><zoom-in /></el-icon>
+						<el-icon @click="handleDownload(file)"><Download /></el-icon>
+						<el-icon @click="handleRemove(file)"><Delete /></el-icon>
+					</span>
+				</div>
+			</template>
+		</el-upload>
+
+		<el-image-viewer v-if="showPreview" :url-list="previewList" @close="showPreview = false" />
+
+		<div class="flex justify-end">
 			<el-button type="primary" color="#1e202514" @click="reset" class="text-black">取消</el-button>
 			<el-button type="primary" color="#2b5aed" @click="createNote">新建笔记</el-button>
 		</div>
@@ -15,15 +41,44 @@ import { ref } from 'vue';
 import { MdEditor } from 'md-editor-v3';
 import 'md-editor-v3/lib/style.css';
 
+import { CircleClose, Delete, Download, Plus, ZoomIn } from '@element-plus/icons-vue';
+import type { UploadFile } from 'element-plus';
+
 import WhiteContainer from '@/components/WhiteContainer.vue';
 
+// 标题
 const rubric = ref<string>('');
+// 内容
 const content = ref<string>('');
+
+// 重置标题
+const resetRubric = () => {
+	rubric.value = '';
+};
 
 // 取消
 const reset = () => {
 	rubric.value = '';
 	content.value = '';
+};
+
+const disabled = ref(false);
+
+const handleRemove = (file: UploadFile) => {
+	console.log(file);
+};
+
+const showPreview = ref(false);
+const previewList = ref<string[]>([]);
+// 点击预览
+const handlePreviewClick = (file: UploadFile) => {
+	console.log(file);
+	previewList.value = [file.url!];
+	showPreview.value = true;
+};
+
+const handleDownload = (file: UploadFile) => {
+	console.log(file);
 };
 
 // 新建笔记
@@ -46,7 +101,7 @@ const createNote = () => {
 	}
 }
 :deep(.md-editor) {
-	height: 25rem;
+	height: 26rem;
 	border: 0;
 	.cm-content {
 		padding: 0;
@@ -57,6 +112,21 @@ const createNote = () => {
 	}
 	.md-editor-footer {
 		border: 0;
+	}
+}
+:deep(.el-upload-list--picture-card) {
+	flex-wrap: nowrap;
+	.el-upload-list__item {
+		width: 6rem;
+		height: 6rem;
+		margin: 0;
+		margin-right: 0.5rem;
+	}
+
+	// 图片选择卡
+	.el-upload--picture-card {
+		width: 6rem;
+		height: 6rem;
 	}
 }
 </style>
